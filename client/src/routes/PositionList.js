91272c7dd1d71424cbfb67371e72eb4e9e8b9c5b
yumeizhong <yumeizhong@ofo.com>
2018-12-20 21:05:34 +0800

    岗位分析页

--- client/src/routes/PositionList.js
+++ client/src/routes/PositionList.js
@@ -3,45 +3,57 @@ import { connect } from 'dva';
 import { Table } from 'antd';
 import request from '../utils/request';
 
-const dataSource = [{
-  key: '1',
-  name: '胡彦斌',
-  age: 32,
-  address: '西湖区湖底公园1号'
-}, {
-  key: '2',
-  name: '胡彦祖',
-  age: 42,
-  address: '西湖区湖底公园1号'
-}];
-
-const columns = [{
-  title: '姓名',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-  key: 'age',
-}, {
-  title: '住址',
-  dataIndex: 'address',
-  key: 'address',
-}];
 
 class PositionList extends Component{
   constructor(props) {
     super(props);
+    this.state = {
+      dataSource: [],
+      columns:
+        [{
+          title: '公司',
+          dataIndex: 'companyFullName',
+          render: (text,record)=>{
+            let h = `https://www.lagou.com/gongsi/${record['companyId']}.html`;
+            return (<a target='blank' href={h}>{text}</a>);
+          }
+        }, {
+          title: '岗位',
+          dataIndex: 'positionName',
+          render:(text, record)=>{
+            let h = `https://www.lagou.com/jobs/${record["positionId"]}.html`;
+            return (<a target='blank' href={h}>{text}</a>);
+          }
+        }, {
+          title: '薪资',
+          dataIndex: 'salary'
+        }, {
+          title: '规模',
+          dataIndex: 'companySize'
+        }, {
+          title: '融资阶段',
+          dataIndex: 'financeStage'
+        }, {
+          title: '城市',
+          dataIndex: 'city'
+        }, {
+          title: '区域',
+          dataIndex: 'district'
+        }]
+    };
   }
 
   componentWillMount() {
     request('http://10.240.3.44:8081/getPositionList').then((data)=>{
       console.log(data);
+      this.setState({
+        dataSource: data.data
+      });
     });
   }
 
   render(){
-    return(<Table dataSource={dataSource} columns={columns} />);
+    return(<Table dataSource={this.state.dataSource} columns={this.state.columns} />);
   }
 }
 
