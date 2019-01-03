a55eb70a078634f7737804807a9d8639039664e3
yumeizhong <yumeizhong@ofo.com>
2019-01-03 15:56:43 +0800

    不同环境的后端地址判断

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,4 +1,8 @@
+let  server = 'http://127.0.0.1:3000'; // 本地环境
+if(window.location.origin == 'http://freego.ofo.com'){
+  server = 'http://10.2.40.110:3000'; //线上环境
+}
+
 export default {
-  server: 'http://127.0.0.1:8081', // 本地环境
-  //server: 'http://10.2.40.110:3000', //线上环境
+  server
 }
